fad5b9a65656ac9d5f201e30a3a178d60169bd0b
Raj Vhora <44524083+RajVhora@users.noreply.github.com>
2020-04-05 12:06:31 +0530

    Initial commit

---
+++ CircletraceSolver.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+class Solver():
+    def Solver(self,obst,start,end,dangle):
+
+        a = obst[0]
+        b = obst[1]
+        r = obst[2]
+
+        c = start[0]
+        d = start[1]
+
+        e = end[0]
+        f = end[1]
+
+        def getarg(x1,y1):
+            temp1 = np.arctan2([y1-b],[x1-a])
+            if temp1 < 0:
+                temp1 = temp1 + 2*np.pi
+            return temp1 * 180/np.pi
+
+        def quadsolver(coeff):
+            a = coeff[0]
+            b = coeff[1]
+            c = coeff[2]
+            d = np.sqrt(b**2 - 4*a*c)
+            y1 = (-b+d)/(2*a)
+            y2 = (-b-d)/(2*a)
+            return y1,y2
+
+        g = -(f-d)
+        h = (e-c)
+        i = (f*c) - (e*d)
+
+        if g != 0:
+            coeff = [(h**2/g**2)+1 ,(((2*h*i)/(g**2)) + ((2*h*a)/g) - (2*b)) ,(((i**2)/(g**2)) + (a**2) + (2*a*i/g) + (b**2) - (r**2))]
+            #y1,y2 = np.roots(coeff)
+            y1,y2 = quadsolver(coeff)
+            print(y1,y2)
+            x1 = -(h*y1 + i)/g
+            x2 = -(h*y2 + i)/g
+        else:
+            y1,y2 = -i/h
+            coeff = [1,-2*a,(((i**2)/(h**2)) + (a**2) + (2*b*i/g) + (b**2) - (r**2))]
+            x1,x2 = quadsolver(coeff)
+
+        def distbetweenpts(x1,y1,x2,y2):
+            return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+        d1 = distbetweenpts(x1,y1,c,d)
+        d2 = distbetweenpts(x2,y2,c,d)
+
+        nearpt = []
+        farpt = []
+        if d1 < d2:
+            nearpt = (x1,y1)
+            farpt = (x2,y2)
+        else:
+            nearpt = (x2,y2)
+            farpt = (x1,y1)
+
+        path = []
+
+        #path.append((c,d))
+
+        path.append((nearpt[0],nearpt[1]))
+
+        theta1 = float(getarg(nearpt[0],nearpt[1]))
+        theta2 = float(getarg(farpt[0],farpt[1]))
+
+        operation = ''
+        if(abs(theta1-theta2)>180):
+            if(theta1>theta2):
+                operation = '+'
+            else:
+                operation = '-'
+        else:
+            if(theta1>theta2):
+                operation = '-'
+            else:
+                operation = '+'
+
+        cangle = theta1
+        eps = 1
+        count = 0
+        while(abs(cangle - theta2)>2):
+            if(operation is '+'):
+                cangle = cangle + eps
+            else:
+                cangle = cangle - eps
+            cangle = cangle%360
+            x = a + r*np.cos(cangle*np.pi/180)
+            y = b + r*np.sin(cangle*np.pi/180)
+            if (count%dangle == 0):
+                path.append((float(x),float(y)))
+            count = count + 1
+        #path.append((float(e),float(f)))
+        return path
---
+++ circletracer.py
@@ -0,0 +1,150 @@
+# Algorithm
+# Accept all points and obstacles
+# Start with first point..
+# Check if path from first point to second lies through any circle(obstacle)
+# if not go straight
+# else
+#   Make a list of obstacles..
+#   go to intersection of first obst and line
+#   grace circle until second intersection point
+#   go straight again until next circle and line intersection (repeat from 6)
+# (You must get till end by this and repeat this for all start-end pairs)
+
+import pygame
+import math
+import sys
+import random
+from math import *
+from pygame import *
+from CircletraceSolver import Solver
+
+blue = 0, 0, 255
+white = 255, 255, 255
+black = 0, 0, 0
+red = 255, 0, 0
+green = 0, 255, 0
+cyan = 0, 180, 105
+maroon = 119, 27, 7
+
+obst1 = [450, 300, 250]
+#obst2 = [435, 300, 25]
+#obst3 = [575, 250, 50]
+#obst4 = [600, 400, 45]
+#obst = [obst1, obst2, obst3, obst4]
+obst = [obst1]
+path = []
+gr = 10     #Grace Radius variables
+
+screen = pygame.display.set_mode((900, 600))
+
+
+def init_obstacles():  # Later run in for loop
+    for q in obst:
+        pygame.draw.circle(screen, maroon, q[0:2], q[2], 0)
+        r = int(q[2])
+        r = r + gr
+        q[2] = r
+
+def reset():
+    screen.fill((240, 240, 0))
+    init_obstacles()
+    pygame.display.update()
+    print("resetting")
+
+
+def dist_between(a, b):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5
+
+
+def OptimisePathDistance(ipos, destiny):
+    finaldestiny = [ipos]
+    while(len(destiny) > 0):
+        dist_row = []
+        for i in destiny:
+            dist_row.append(dist_between(i, finaldestiny[-1]))
+        mini = sorted(dist_row)
+        nearwaypt = destiny[dist_row.index(mini[0])]
+        finaldestiny.append(nearwaypt)
+        # now remove nearwaypt from destiny
+        destiny.remove(nearwaypt)
+    return finaldestiny
+
+
+def sortedobstacles(obstacles, start):
+    distances = []
+    for o in obstacles:
+        d = dist_between(o, start)
+        distances.append(d)
+    distances1 = sorted(distances)
+    sl = []
+    for i in range(0, len(obstacles)):
+        sl.append(obstacles[distances.index(distances1[i])])
+    return sl
+
+
+def f(p,q,x1,y1,x2,y2):
+	t = ((y1-q)*(y2-y1)+(x2-x1)*(x1-p))*((y2-q)*(y2-y1)+(x2-x1)*(x2-p))
+	if t<0:
+		return True
+	else:
+		return False
+
+def inmypath(obst,s,e):
+	trouble = []
+	for o in obst:
+		if(f(o[0],o[1],s[0],s[1],e[0],e[1]) == False):
+			print(o,"NOT OBSTACLE")
+		else :
+			distance = ((e[1]-s[1])*o[0] - (e[0]-s[0])*o[1] + e[0]*s[1] - e[1]*s[0])/dist_between(s,e)
+			if(abs(distance)<o[2]):
+				print(o,"As OBSTACLE")
+				trouble.append(o)
+	return trouble
+
+def findpath(start, end):
+    global obst,gr
+    sol = Solver()
+    obstacles = inmypath(obst, start, end)
+    path = [start]
+    if len(obstacles) > 0:
+        obstacles = sortedobstacles(obstacles, start)
+        for i in obstacles:
+            dangle = floor(acos((i[2] - gr)/i[2])*180/pi)
+            temp = sol.Solver(i, path[-1], end,dangle)
+            path = path + temp
+    path.append(end)
+    if len(obstacles) == 0:
+        path.append(end)
+    return path
+
+
+screen.fill((240, 240, 0))
+init_obstacles()
+pygame.display.update()
+
+goalset = 0
+destiny = []
+n = int(input("Enter the number of waypoints to visit:"))
+while True:
+    while goalset < n:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                position = event.pos
+                print(position)
+                destiny.append(position)
+                if goalset == 0:
+                    start = position
+                pygame.draw.circle(screen, red, position, 6, 0)
+                pygame.display.update()
+                goalset += 1
+            if event.type == pygame.QUIT:
+                pygame.quit()
+    destiny = OptimisePathDistance(start, destiny[1:n])
+    for i in range(0, len(destiny)-1):
+        path = findpath(destiny[i], destiny[i+1])
+        print(path)
+        for i in range(len(path)-1):
+            pygame.draw.line(screen, cyan, path[i], path[i+1])
+            pygame.display.update()
+    goalset = 0
+    destiny = []
